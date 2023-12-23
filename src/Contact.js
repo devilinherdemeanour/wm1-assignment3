518983250d2358b236f2aa21782c55711593973f
Ibrahim Mammadli <rare@Ibrahims-MacBook-Air-2.local>
2023-12-23 10:25:24 +0400

    contact me page

--- src/Contact.js
+++ src/Contact.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import styles from './styles/contact.module.scss'
 
 const Contact = () => {
   return (
-    <div>
-     <form>
-        <label>Name and Surname</label>
-        <input type='text'/> <br/>
-        <label>E-mail</label>
-        <input type='text'/> <br/>
-        <label>Subject</label>
-        <input type='text'/> <br/>
-        <label>Message</label>
-        <input type='text'/>
-     </form>
+    <div className={styles.container}>
+      <div className={styles.contact}>
+        <form>
+          <div className={styles.item}>
+            <label>Name and Surname</label>
+            <input type='text'/> <br/>
+          </div>
+          <div className={styles.item}>
+            <label>E-mail</label>
+            <input type='text'/> <br/>
+          </div>
+          <div className={styles.item}>
+            <label>Subject</label>
+            <input type='text'/> <br/>
+          </div>
+          <div className={styles.item}>
+            <label>Message</label>
+            <input type='text'/>
+          </div>
+
+          <button>Send message</button>
+        </form>
+      </div>
     </div>
   );
 };
